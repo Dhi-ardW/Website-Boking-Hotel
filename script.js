1cb66a7824a8ae3705379774daf5f0a3a91c07ac
Ard <78478158+RBARAB@users.noreply.github.com>
2025-08-16 12:16:08 +0700

    membuat register page dan login page

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ links.forEach((link) => {
     this.classList.add("active");
   });
 });
+
+// tombol lihat password
+
+const eye = document.querySelector("#eye");
+const eyes = document.getElementById("eyes");
+
+eye.addEventListener("click", function () {
+  const password = document.getElementById("password");
+
+  if (password.type === "password") {
+    password.type = "text";
+    eyes.classList.add("bi-eye-slash-fill");
+    eyes.classList.remove("bi-eye-fill");
+  } else {
+    password.type = "password";
+    eyes.classList.remove("bi-eye-slash-fill");
+    eyes.classList.add("bi-eye-fill");
+  }
+});
